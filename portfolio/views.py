912604c6f71e3d67213145a4bf43483bbc723131
suman <beingsuman10@gmail.com>
2024-07-26 17:39:02 +0545

    suman

---
+++ portfolio/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+
+def contact(request):
+    return render(request, 'portfolio/contact.html')
+
+
+
+
